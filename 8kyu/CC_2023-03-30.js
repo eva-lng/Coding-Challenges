41ca3e826e2418e5b719a99ed9a54976b5e01780
Eva Langerova <langerova_eva@yahoo.com>
2023-03-30 18:32:25 +0200

    Add coding challenge for 30-3-23 coding challenge

---
+++ 8kyu/CC_2023-03-30.js
@@ -0,0 +1,15 @@
+/* Find the smallest integer in the array
+
+Given an array of integers your solution should find the smallest integer. You can assume, for the purpose of this kata, that the supplied array will not be empty. */
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      let num = args[0];
+      for (let i = 0; i < args.length; i++){
+        if (args[i] < num) {
+          num = args[i];
+        }
+      }
+      return num;
+    }
+}
