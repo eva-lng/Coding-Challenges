70096919ebfe6130ff19a52351265eb9b33ae9d3
Eva Langerova <langerova_eva@yahoo.com>
2023-04-10 18:38:12 +0200

    Add coding challenge for 10-4-23 coding challenge

---
+++ 8kyu/CC_2023-04-10.js
@@ -0,0 +1,22 @@
+/* Keep Hydrated!
+
+Nathan loves cycling. Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+For example:
+time = 3 ----> litres = 1
+time = 6.7---> litres = 3
+time = 11.8--> litres = 5 */
+
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+/* Binary Addition
+
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+The binary number returned should be a string. */
+
+function addBinary(a,b) {
+  return (a + b).toString(2);
+}
