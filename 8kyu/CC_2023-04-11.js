e006abe5281910b9ed16f34c4c03a1c96804f001
Eva Langerova <langerova_eva@yahoo.com>
2023-04-11 18:38:39 +0200

    Add coding challenge for 11-4-23 coding challenge

---
+++ 8kyu/CC_2023-04-11.js
@@ -0,0 +1,19 @@
+/* Opposites Attract
+
+Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't. */
+
+function lovefunc(flower1, flower2){
+    return (flower1 + flower2) % 2 !== 0;
+}
+
+/* DNA to RNA Conversion
+
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA. */
+
+function DNAtoRNA(dna) {
+    return dna.replaceAll('T', 'U');
+}
