aa2ce950b78efbca73dc2d677f4cbc9d240d1c54
Eva Langerova <langerova_eva@yahoo.com>
2023-04-21 18:33:17 +0200

    Add coding challenge for 21-4-23 coding challenge

---
+++ 8kyu/CC_2023-04-21.1.js
@@ -0,0 +1,20 @@
+/* Sum without highest and lowest number
+
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ). The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+Example:
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+Input validation:
+If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0. */
+
+function sumArray(array) {
+    if (!array || array.length <= 2) {
+      return 0;
+    }
+    
+    let sortedArr = array.sort((a, b) => a - b);
+    let sum = sortedArr.slice(1, -1).reduce((sum, n) => sum + n, 0);
+    
+    return sum;
+}
