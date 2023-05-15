a8c3401a911142bec414fa87df76d7d88f6fb47b
Eva Langerova <langerova_eva@yahoo.com>
2023-05-15 13:34:55 +0200

    Add coding challenge for 15-5-23 coding challenge

---
+++ 7kyu/CC_2023-05-15.js
@@ -0,0 +1,58 @@
+/* 99 Bottles of Beer
+
+Complete the function that returns the lyrics for the song 99 Bottles of Beer as an array of strings: each line should be a separate element - see the example at the bottom.
+
+Note: in order to avoid hardcoded solutions, the size of your code is limited to 1000 characters
+
+Lyrics:
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+
+98 bottles of beer on the wall, 98 bottles of beer.
+Take one down and pass it around, 97 bottles of beer on the wall.
+
+...and so on...
+
+3 bottles of beer on the wall, 3 bottles of beer.
+Take one down and pass it around, 2 bottles of beer on the wall.
+
+2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+
+1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+
+Example:
+[ "99 bottles of beer on the wall, 99 bottles of beer.",
+  "Take one down and pass it around, 98 bottles of beer on the wall.",
+  "98 bottles of beer on the wall, 98 bottles of beer.",
+
+  ...and so on...
+
+  "3 bottles of beer on the wall, 3 bottles of beer.",
+  "Take one down and pass it around, 2 bottles of beer on the wall.",
+  "2 bottles of beer on the wall, 2 bottles of beer.",
+  "Take one down and pass it around, 1 bottle of beer on the wall.",
+  "1 bottle of beer on the wall, 1 bottle of beer.",
+  "Take one down and pass it around, no more bottles of beer on the wall.",
+  "No more bottles of beer on the wall, no more bottles of beer.",
+  "Go to the store and buy some more, 99 bottles of beer on the wall." ] */
+
+var sing = function () {
+    let lyrics = [];
+    for (let i = 99; i > 2; i--) {
+      lyrics.push(`${i} bottles of beer on the wall, ${i} bottles of beer.`);
+      lyrics.push(`Take one down and pass it around, ${i - 1} bottles of beer on the wall.`);
+    }
+    return lyrics.concat([
+      '2 bottles of beer on the wall, 2 bottles of beer.',
+      'Take one down and pass it around, 1 bottle of beer on the wall.',
+      '1 bottle of beer on the wall, 1 bottle of beer.',
+      'Take one down and pass it around, no more bottles of beer on the wall.',
+      'No more bottles of beer on the wall, no more bottles of beer.',
+      'Go to the store and buy some more, 99 bottles of beer on the wall.'
+    ]);
+};
