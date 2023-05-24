e1935f4bf38fd570c9835a24c7a95cea4a6066fd
Eva Langerova <langerova_eva@yahoo.com>
2023-05-24 20:59:06 +0200

    Add coding challenge for 24-5-23 coding challenge

---
+++ 7kyu/CC_2023-05-24.js
@@ -0,0 +1,15 @@
+/* Reverse a Number
+
+Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321). Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+
+Examples:
+123 -> 321
+-456 -> -654
+1000 -> 1
+*/
+
+function reverseNumber(n) {
+    let reversedStr = Math.abs(n).toString().split('').reverse().join('');
+    let result = Number(reversedStr);
+    return n > 0 ? result : -result;
+}
