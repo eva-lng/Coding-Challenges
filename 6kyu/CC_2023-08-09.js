234f4e25ef502372e879cb904b8f6752755929ae
Eva Langerova <langerova_eva@yahoo.com>
2023-08-09 15:46:43 +0200

    Updated coding challenge

--- 6kyu/CC_2023-08-09.js
+++ 6kyu/CC_2023-08-09.js
@@ -3,13 +3,13 @@
 */
 
 // Solution 1
-function inArray(array1,array2){
+function inArray1(array1,array2){
   const str = array2.join(' ')
   return array1.filter(el => str.includes(el)).sort()
 }
 
 // Solution 2
-function inArray(array1,array2){
+function inArray2(array1,array2){
   const result = []
     
   for (const word of array2) {
