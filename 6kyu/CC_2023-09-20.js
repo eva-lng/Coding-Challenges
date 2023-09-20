db27433402f60ab20b909352aa1636a3c594bce5
Eva Langerova <langerova_eva@yahoo.com>
2023-09-20 18:07:25 +0200

    added coding challenge

---
+++ 6kyu/CC_2023-09-20.js
@@ -0,0 +1,12 @@
+/* Reverse every other word in the string
+
+Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+*/
+
+function reverse(str){
+  return str
+    .split(' ')
+    .map((el, i) => i % 2 === 0 ? el : el.split('').reverse().join(''))
+    .join(' ')
+    .trim()
+}
