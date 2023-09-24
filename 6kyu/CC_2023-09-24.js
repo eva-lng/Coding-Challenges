a161fd1774f6019a65785e62e8892659738015fc
Eva Langerova <langerova_eva@yahoo.com>
2023-09-24 14:34:09 +0200

    added coding challenge

---
+++ 6kyu/CC_2023-09-24.js
@@ -0,0 +1,15 @@
+/* Is Integer Array?
+
+Write a function with the signature shown below:
+
+function isIntArray(arr) {
+  return true
+}
+returns true  / True if every element in an array is an integer or a float with no decimals.
+returns true  / True if array is empty.
+returns false / False for every other input.
+*/
+
+function isIntArray(arr) {
+  return Array.isArray(arr) && arr.every(n => Math.floor(n) === n)
+}
