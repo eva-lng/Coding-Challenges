e190a21982d4636c17d404367551d1b6d437388f
Eva Langerova <langerova_eva@yahoo.com>
2023-10-09 18:10:26 +0200

    updated challenge description

--- 6kyu/CC_2023-10-09.js
+++ 6kyu/CC_2023-10-09.js
@@ -6,7 +6,7 @@ You want to create secret messages which can be deciphered by the Decipher this!
 2. You need to encrypt each word in the message using the following rules:
     - The first letter must be converted to its ASCII code.
     - The second letter must be switched with the last letter
-3.Keepin' it simple: There are no special characters in the input.
+3. Keepin' it simple: There are no special characters in the input.
 
 Examples:
 encryptThis("Hello") === "72olle"
