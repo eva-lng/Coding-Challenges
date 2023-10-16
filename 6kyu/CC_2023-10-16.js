76e35a0539f53337183c4602626d05df9cd1645f
Eva Langerova <langerova_eva@yahoo.com>
2023-10-16 13:29:01 +0200

    added coding challenge

---
+++ 6kyu/CC_2023-10-16.js
@@ -0,0 +1,43 @@
+/* CamelCase to underscore
+
+You wrote all your unit test names in camelCase. But some of your colleagues have troubles reading these long test names. So you make a compromise to switch to underscore separation.
+
+To make these changes fast you wrote a class to translate a camelCase name into an underscore separated name. Implement the ToUnderscore() method.
+
+Example:
+"ThisIsAUnitTest" => "This_Is_A_Unit_Test"
+
+But of course there are always special cases...
+
+You also have some calculation tests. Make sure the results don't get split by underscores. So only add an underscore in front of the first number.
+
+Also some people already used underscore names in their tests. You don't want to change them. But if they are not split correct you should adjust them.
+
+Some of your colleagues mark their tests with a leading and trailing underscore. Don't remove this.
+
+And of course you should handle empty strings to avoid unnecessary errors. Just return an empty string then.
+
+Example:
+"Calculate15Plus5Equals20" => "Calculate_15_Plus_5_Equals_20"
+"This_Is_Already_Split_Correct" => "This_Is_Already_Split_Correct"
+"ThisIs_Not_SplitCorrect" => "This_Is_Not_Split_Correct"
+"_UnderscoreMarked_Test_Name_" => _Underscore_Marked_Test_Name_"
+*/
+
+const toUnderScore = name => {
+  const upperC = 'ABCDEFGHIJKLMNOPQRSTUVWXQYZ'
+  const numbers = '1234567890'
+  return name
+    .split('')
+    .map((el, i, arr) => {
+      if (
+        (i !== 0 && upperC.includes(el) && arr[i-1] !== '_') ||
+        (i !== 0 && numbers.includes(el) && !numbers.includes(arr[i-1]) && arr[i-1] !== '_')
+      ) {
+        return '_' + el
+      } else {
+        return el
+      }
+    })
+    .join('')
+}
