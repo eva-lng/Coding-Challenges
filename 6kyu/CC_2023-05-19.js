cbbd8d83d342f3fc6771471393fc97dd49306861
Eva Langerova <langerova_eva@yahoo.com>
2023-10-22 19:06:31 +0200

    updated coding challenge

--- 6kyu/CC_2023-05-19.js
+++ 6kyu/CC_2023-05-19.js
@@ -6,14 +6,15 @@ arrayDiff([1,2],[1]) == [2]
 
 If a value is present in b, all of its occurrences must be removed from the other:
 
-arrayDiff([1,2,2,2,3],[2]) == [1,3] */
+arrayDiff([1,2,2,2,3],[2]) == [1,3]
+*/
 
 function arrayDiff(a, b) {
-    let result = [];
-    a.forEach(n => {
-      if (!b.includes(n)) {
-        result.push(n);
-      }
-    })
-    return result;
+  let result = [];
+  a.forEach(n => {
+    if (!b.includes(n)) {
+      result.push(n);
+    }
+  })
+  return result;
 }
