ccca5ea9cd6dc5799f7fd431f303e36bf47de157
Eva Langerova <langerova_eva@yahoo.com>
2023-10-24 19:22:11 +0200

    updated coding challenge

--- 8kyu/CC_2023-03-27.js
+++ 8kyu/CC_2023-03-27.js
@@ -1,7 +1,8 @@
 /* Convert a Number to a String!
 
-We need a function that can transform a number (integer) into a string. */
+We need a function that can transform a number (integer) into a string.
+*/
 
 function numberToString(num) {
-    return num.toString();
+  return num.toString();
 }
