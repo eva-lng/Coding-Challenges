3e430fd715c78947a7599c0cb84a89e1ebcda8ba
Eva Langerova <langerova_eva@yahoo.com>
2023-11-02 12:12:59 -0700

    updated coding challenge

--- 8kyu/CC_2023-04-23.2.js
+++ 8kyu/CC_2023-04-23.2.js
@@ -6,16 +6,17 @@ Examples(Operator, value1, value2) --> output:
 ('+', 4, 7) --> 11
 ('-', 15, 18) --> -3
 ('*', 5, 5) --> 25
-('/', 49, 7) --> 7 */
+('/', 49, 7) --> 7
+*/
 
 function basicOp(operation, value1, value2) {
-    if (operation === '+') {
-      return value1 + value2;
-    } else if (operation === '-') {
-      return value1 - value2;
-    } else if (operation === '*') {
-      return value1 * value2;
-    } else if (operation === '/') {
-      return value1 / value2;
-    }
+  if (operation === '+') {
+    return value1 + value2;
+  } else if (operation === '-') {
+    return value1 - value2;
+  } else if (operation === '*') {
+    return value1 * value2;
+  } else if (operation === '/') {
+    return value1 / value2;
+  }
 }
