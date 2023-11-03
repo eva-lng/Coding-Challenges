18bd5d259b1fd569f4b35866005c2b59641bd648
Eva Langerova <langerova_eva@yahoo.com>
2023-11-03 12:14:16 -0700

    updated coding challenge

--- 8kyu/CC_2023-04-25.2.js
+++ 8kyu/CC_2023-04-25.2.js
@@ -9,9 +9,9 @@ Examples (Input -> Output):
 */
 
 function doubleChar(str) {
-    let result = '';
-    for (let i = 0; i < str.length; i++) {
-      result += str[i] + str[i];
-    }
-    return result;
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    result += str[i] + str[i];
+  }
+  return result;
 }
