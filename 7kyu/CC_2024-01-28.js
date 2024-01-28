9bf82cb010643ae8a03d3ffad2e10b60ed1d0bf1
Eva Langerova <langerova_eva@yahoo.com>
2024-01-28 10:03:20 +0100

    added coding challenge

---
+++ 7kyu/CC_2024-01-28.js
@@ -0,0 +1,34 @@
+/* Maximum Multiple
+
+Given a Divisor and a Bound, find the largest integer N, such That:
+- N is divisible by divisor
+- N is less than or equal to bound
+- N is greater than 0
+
+Notes:
+- The parameters (divisor, bound) passed to the function are only positive values .
+- It's guaranteed that a divisor is Found .
+
+Input >> Output Examples:
+divisor = 2, bound = 7 ==> return (6)
+Explanation:
+(6) is divisible by (2) , (6) is less than or equal to bound (7) , and (6) is > 0 .
+
+divisor = 10, bound = 50 ==> return (50)
+Explanation:
+(50) is divisible by (10) , (50) is less than or equal to bound (50) , and (50) is > 0 .*
+
+divisor = 37, bound = 200 ==> return (185)
+Explanation:
+(185) is divisible by (37) , (185) is less than or equal to bound (200) , and (185) is > 0 .
+*/
+
+// solution 1
+function maxMultiple1(divisor, bound) {
+  return bound - bound % divisor;
+}
+
+// solution 2
+function maxMultiple2(divisor, bound) {
+  return Math.floor(bound / divisor) * divisor;
+}
