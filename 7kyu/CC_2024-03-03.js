3c954e786c202cc1602d317f71f1108d02e1bb41
Eva Langerova <langerova_eva@yahoo.com>
2024-03-03 17:32:30 +0100

    added coding challenge

---
+++ 7kyu/CC_2024-03-03.js
@@ -0,0 +1,14 @@
+/* Switcheroo
+
+Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+Example:
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+*/
+
+function switcheroo(x){
+  return [...x]
+    .map(el => el === 'a' ? 'b' : el === 'b' ? 'a' : 'c')
+    .join('')
+}
