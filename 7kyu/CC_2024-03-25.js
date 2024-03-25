c1b33006ac5ca550116bf04677d179d5ae668766
Eva Langerova <langerova_eva@yahoo.com>
2024-03-25 19:40:46 +0100

    added coding challenge

---
+++ 7kyu/CC_2024-03-25.js
@@ -0,0 +1,14 @@
+/* Working with arrays
+
+In this kata the function returns an array/list of numbers without its last element. The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+
+function withoutLast(arr) {
+  // Fix it
+  arr.pop(); // removes the last element
+  return arr;
+}
+*/
+
+function withoutLast(arr) {
+  return arr.slice(0, -1);
+}
