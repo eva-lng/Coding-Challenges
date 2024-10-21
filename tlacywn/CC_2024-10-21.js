9cdf518f98a6bb78fabccfe276e1d7eca935a5c0
Eva Langerova <langerova_eva@yahoo.com>
2024-10-21 10:17:55 -0400

    add coding challenge

---
+++ tlacywn/CC_2024-10-21.js
@@ -0,0 +1,16 @@
+/* Bubble Sort
+
+Implement bubble sort.
+*/
+
+function bubble_sort(arr) {
+  for (let i = 0; i < arr.length; ++i) {
+    for (let j = 0; j < arr.length - 1 - i; ++j) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
