21f24d0eb6b1c56cd9b5ad1e928155b9508fc0f3
Eva Langerova <langerova_eva@yahoo.com>
2024-10-23 14:31:05 -0500

    add coding challenge

--- tlacywn/CC_2024-10-22.js
+++ tlacywn/CC_2024-10-22.js
@@ -4,7 +4,7 @@ Implement quickosort
 */
 
 // solution 1
-function quickSort(arr) {
+function quickSort1(arr) {
   if (arr.length <= 1) {
     return arr;
   }
@@ -22,4 +22,39 @@ function quickSort(arr) {
   }
 
   return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+}
+
+// solution 2
+function qs(arr, lo, hi) {
+  if (lo >= hi) {
+    return;
+  }
+
+  const pivotIdx = partition(arr, lo, hi);
+  qs(arr, lo, pivotIdx - 1);
+  qs(arr, pivotIdx - 1, hi);
+}
+
+function partition(arr, lo, hi) {
+  const pivot = arr[hi];
+  let idx = lo - 1;
+
+  for (let i = lo; i < hi; ++i) {
+    if (arr[i] <= pivot) {
+      idx++;
+      const tmp = arr[i];
+      arr[i] = arr[idx];
+      arr[idx] = tmp;
+    }
+  }
+
+  idx++;
+  arr[hi] = arr[idx];
+  arr[idx] = pivot;
+
+  return idx;
+}
+
+function quickSort2(arr) {
+  qs(arr, 0, arr.length - 1);
 }
