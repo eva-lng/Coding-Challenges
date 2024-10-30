698920aab437ebe194acd3743683cfcaae65bec1
Eva Langerova <langerova_eva@yahoo.com>
2024-10-30 10:15:17 -0500

    add coding challenge

---
+++ neetcode/CC_2024-10-30.js
@@ -0,0 +1,15 @@
+/* Is Anagram
+
+
+*/
+
+class Solution {
+  /**
+   * @param {string} s
+   * @param {string} t
+   * @return {boolean}
+   */
+  isAnagram(s, t) {
+      return s.split('').sort().join('') === t.split('').sort().join('');
+  }
+}
