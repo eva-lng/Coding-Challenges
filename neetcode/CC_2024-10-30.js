723064a5a3d3d8b03e8a94c65fc598e5fa5421de
Eva Langerova <langerova_eva@yahoo.com>
2024-10-30 10:21:58 -0500

    add coding challenge

--- neetcode/CC_2024-10-30.js
+++ neetcode/CC_2024-10-30.js
@@ -9,7 +9,26 @@ class Solution {
    * @param {string} t
    * @return {boolean}
    */
-  isAnagram(s, t) {
+  isAnagram1(s, t) {
+    if (s.length !== t.length) return false;
+
+    const charCount = {};
+
+    for (let char of s) {
+      charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    for (let char of t) {
+      if (!charCount[char]) {
+        return false;
+      }
+      charCount[char]--;
+    }
+
+    return true;
+  }
+
+  isAnagram2(s, t) {
       return s.split('').sort().join('') === t.split('').sort().join('');
   }
 }
