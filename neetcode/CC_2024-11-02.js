be00fde2235fd51ff8ce07edfeb9cebdd7ed3228
Eva Langerova <langerova_eva@yahoo.com>
2024-11-02 15:57:02 -0500

    update coding challenge

--- neetcode/CC_2024-11-02.js
+++ neetcode/CC_2024-11-02.js
@@ -26,8 +26,26 @@ class Solution {
    * @return {string[][]}
    */
 
+  // hash table
+  groupAnagrams1(strs) {
+    const res = {};
+
+    for (let s of strs) {
+      const count = new Array(26).fill(0);
+      for (let c of s) {
+        count[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+      }
+      const key = count.join(',');
+      if (!res[key]) {
+        res[key] = [];
+      }
+      res[key].push(s);
+    }
+    return Object.values(res);
+  }
+
   // sorting
-  groupAnagrams(strs) {
+  groupAnagrams2(strs) {
     const res = {};
 
     for (let s of strs) {
