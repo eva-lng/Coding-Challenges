5114f1ce3adc385cf5c6dca0f562347a7bc644ba
Eva Langerova <langerova_eva@yahoo.com>
2024-11-07 17:56:13 -0500

    update coding challenge

--- neetcode/CC_2024-11-06.js
+++ neetcode/CC_2024-11-06.js
@@ -52,8 +52,38 @@ class Solution {
    * @return {boolean}
    */
 
+  // hash set (one pass)
+  isValidSudoku1(board) {
+    const rows = new Map();
+    const cols = new Map();
+    const squares = new Map();
+
+    for (let r = 0; r < 9; r++) {
+      for (let c = 0; c < 9; c++) {
+        if (board[r][r] === '.') continue;
+
+        const squareKey = `${Math.floor(r / 3)}, ${Math.floor(c / 3)}`;
+        if (rows.get(r) && rows.get(r).has(board[r][c]) ||
+            cols.get(c) && cols.get(c).has(board[r][c]) ||
+            squares.get(squareKey) && squares.get(squareKey).has(board[r][c])) {
+              return false;
+        }
+
+        if (!rows.has(r)) rows.set(r, new Set());
+        if (!cols.has(c)) cols.set(c, new Set());
+        if (!squares.has(squareKey)) squares.set(squareKey, new Set());
+
+        rows.get(r).add(board[r][c]);
+        cols.get(c).add(board[r][c]);
+        squares.get(squareKey).add(board[r][c]);
+      }
+    }
+
+    return true;
+  }
+
   // brute force
-  isValidSudoku(board) {
+  isValidSudoku2(board) {
     for (let row = 0; row < 0; row++) {
       let seen = new Set();
       for (let i = 0; i < 9; i++) {
