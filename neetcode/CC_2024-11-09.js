3e41d2ec6516770797359067d588f6d11f36afb6
Eva Langerova <langerova_eva@yahoo.com>
2024-11-09 08:30:25 +0100

    add coding challenge

---
+++ neetcode/CC_2024-11-09.js
@@ -0,0 +1,62 @@
+/* Two Integer Sum II
+
+Given an array of integers numbers that is sorted in non-decreasing order.
+
+Return the indices (1-indexed) of two numbers, [index1, index2], such that they add up to a given target number target and index1 < index2. Note that index1 and index2 cannot be equal, therefore you may not use the same element twice.
+
+There will always be exactly one valid solution.
+
+Your solution must use O(1) additional space.
+
+Example 1:
+Input: numbers = [1,2,3,4], target = 3
+Output: [1,2]
+
+Explanation:
+The sum of 1 and 2 is 3. Since we are assuming a 1-indexed array, index1 = 1, index2 = 2. We return [1, 2].
+
+Constraints:
+2 <= numbers.length <= 1000
+-1000 <= numbers[i] <= 1000
+-1000 <= target <= 1000
+*/
+
+class Solution {
+  /**
+   * @param {number[]} numbers
+   * @param {number} target
+   * @return {number[]}
+   */
+
+  // binary search
+  twoSum(numbers, target) {
+    for ( let i = 0; i < numbers.length; i++) {
+      let l = i + 1;
+      let r = numbers.length - 1;
+      let tmp = target - numbers[i];
+      while (l <= r) {
+        let mid = l + Math.floor((r - l) / 2);
+        if (numbers[mid] === tmp) {
+          return [i + 1, mid + 1];
+        } else if (numbers[mid] < tmp) {
+          l = mid + 1;
+        } else {
+          r = mid - 1;
+        }
+      }
+    }
+    return [];
+  }
+
+  // brute force
+  twoSum2(numbers, target) {
+    for (let i = 0; i < numbers.length; i++) {
+      for (let j = 0; j < numbers.length; j++) {
+        if (numbers[i] + numbers[j] === target) {
+          return [i + 1, j + 1];
+        }
+      }
+    }
+    return [];
+  }
+}
