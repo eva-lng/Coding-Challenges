c1cbb7ec8420d22ed80e1f837b7ce2c79c24660a
Eva Langerova <langerova_eva@yahoo.com>
2025-02-01 17:39:42 +0100

    add coding challenge

---
+++ neetcode/CC_2025-02-01.js
@@ -0,0 +1,41 @@
+/* Longest Common Prefix
+
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+Constraints:
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters if it is non-empty.
+*/
+
+class Solution {
+  /**
+   * @param {string[]} strs
+   * @return {string}
+   */
+
+  longestCommonPrefix(strs) {
+    const arr = strs.slice().sort();
+    let res = "";
+
+    for (let i = 0; i < arr[0].length; i++) {
+      if (arr[0][i] === arr[arr.length - 1][i]) {
+        res += arr[0][i];
+      } else {
+        break;
+      }
+    }
+    return res;
+  }
+}
