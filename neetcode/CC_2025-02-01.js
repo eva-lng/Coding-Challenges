0fa5b64636762f284647cbf194df2489ac75af2f
Eva Langerova <langerova_eva@yahoo.com>
2025-02-01 17:49:05 +0100

    update coding challenge

--- neetcode/CC_2025-02-01.js
+++ neetcode/CC_2025-02-01.js
@@ -25,7 +25,22 @@ class Solution {
    * @return {string}
    */
 
-  longestCommonPrefix(strs) {
+  longestCommonPrefix1(strs) {
+    let pref = strs[0];
+    let prefLen = pref.length;
+
+    for (let i = 1; i < strs.length; i++) {
+      const s = strs[i];
+      while (pref !== s.substring(0, prefLen)) {
+        prefLen--;
+        if (prefLen === 0) return "";
+        pref = pref.substring(0, prefLen);
+      }
+    }
+    return pref;
+  }
+
+  longestCommonPrefix2(strs) {
     const arr = strs.slice().sort();
     let res = "";
 
