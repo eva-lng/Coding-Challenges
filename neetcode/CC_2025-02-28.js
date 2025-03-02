f72f1e74cefe8109c1d7257a3269396f998eaa99
Eva Langerova <langerova_eva@yahoo.com>
2025-03-02 15:02:00 +0100

    update coding challenge

--- neetcode/CC_2025-02-28.js
+++ neetcode/CC_2025-02-28.js
@@ -77,4 +77,12 @@ class Solution {
     }
     return row;
   }
+
+  getRow5(rowIndex) {
+    const row = [1];
+    for (let i = 1; i <= rowIndex; i++) {
+      row.push(Math.floor((row[row.length - 1] * (rowIndex - i + 1)) / i));
+    }
+    return row;
+  }
 }
