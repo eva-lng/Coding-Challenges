ad839e16947243e534c919e2c64daa9096ff7089
Eva Langerova <langerova_eva@yahoo.com>
2025-03-15 09:21:56 +0100

    add coding challenge

---
+++ neetcode/CC_2025-03-15.js
@@ -0,0 +1,39 @@
+/* Contains Duplicate II
+
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+Example 1:
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+Example 2:
+Input: nums = [1,0,1,1], k = 1
+Output: true
+
+Example 3:
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+
+Constraints:
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+0 <= k <= 105
+*/
+
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @param {number} k
+   * @return {boolean}
+   */
+
+  // brute force
+  containsNearbyDuplicate(nums, k) {
+    for (let l = 0; l < nums.length; l++) {
+      for (let r = l + 1; r < Math.min(nums.length, l + k + 1); r++) {
+        if (nums[l] === nums[r]) return true;
+      }
+    }
+    return false;
+  }
+}
