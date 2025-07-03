c3036422c38833b0d8f4e561e61093dffe71a09f
Eva Langerova <langerova_eva@yahoo.com>
2025-07-03 19:00:30 +0200

    add co coding challenge

---
+++ codewars/Java/8kyu/CC_2025-07-03b.java
@@ -0,0 +1,15 @@
+/* Opposite number
+
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+1: -1
+14: -14
+-34: 34
+*/
+
+public class Kata {
+  public static int opposite(int number) {
+    return -number;
+  }
+}
