7bd1c902c35a914ce70fe22e5c1698486de099dc
Eva Langerova <langerova_eva@yahoo.com>
2025-07-05 19:12:36 +0200

    add co coding challenge

---
+++ codewars/Java/8kyu/CC_2025-07-05b.java
@@ -0,0 +1,18 @@
+/* Convert a Number to a String!
+
+
+*/
+
+class Kata {
+  public static String numberToString1(int num) {
+    return String.valueOf(num);
+  }
+
+  public static String numberToString2(int num) {
+    return Integer.toString(num);
+  }
+
+  public static String numberToString3(int num) {
+    return "" + num;
+  }
+}
