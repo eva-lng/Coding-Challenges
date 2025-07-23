34696de889849838ee78f6fb031d7f26821e30e1
Eva Langerova <langerova_eva@yahoo.com>
2025-07-23 18:54:59 +0200

    add coding challenge

---
+++ codewars/Java/8kyu/CC_2025-07-23b.java
@@ -0,0 +1,10 @@
+/* You Can't Code Under Pressure #1
+
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+class Java {
+  public static int doubleInteger(int i) {
+    return 2 * i;
+  }
+}
