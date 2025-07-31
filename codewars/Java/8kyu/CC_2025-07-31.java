095b3bb5d4badf25d4cba55f1c21cf0dc78baf2c
Eva Langerova <langerova_eva@yahoo.com>
2025-07-31 19:27:11 +0200

    update coding challenge

--- codewars/Java/8kyu/CC_2025-07-31.java
+++ codewars/Java/8kyu/CC_2025-07-31.java
@@ -7,11 +7,17 @@ a can contain numbers or strings. x can be either.
 Return true if the array contains the value, false if not.
 */
 
+import java.util.Arrays;
+
 public class Solution {
-  public static boolean check(Object[] a, Object x) {
+  public static boolean check1(Object[] a, Object x) {
     for (Object o: a) {
       if (o.equals(x)) return true;
     }
     return false;
+  }
+
+  public static boolean check2(Object[] a, Object x) {
+    return Arrays.asList(a).contains(x);
   }
 }
