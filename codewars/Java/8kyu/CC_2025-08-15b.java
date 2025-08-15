9d3ea6992d82adb97e66f129e9746f1b819a551d
Eva Langerova <langerova_eva@yahoo.com>
2025-08-15 19:38:17 +0200

    add coding challenge

---
+++ codewars/Java/8kyu/CC_2025-08-15b.java
@@ -0,0 +1,18 @@
+/* Fake Binary
+
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+*/
+
+public class FakeBinary {
+  public static String fakeBin(String numberString) {
+    String[] arr = numberString.split("");
+    String res = "";
+    for (String s: arr) {
+      if (Integer.valueOf(s) < 5) res += "0";
+      else res += "1";
+    }
+    return res;
+  }
+}
