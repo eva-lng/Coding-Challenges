c18fb120a94b5efda745f150fa747fe2b690bcee
Eva Langerova <langerova_eva@yahoo.com>
2025-08-17 19:42:48 +0200

    add coding challenge

---
+++ codewars/Java/8kyu/CC_2025-08-17.java
@@ -0,0 +1,16 @@
+/* Reversed sequence 
+
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+*/
+
+public class Sequence {
+  public static int[] reverse(int n) {
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = n - i;
+    }
+    return arr;
+  }
+}
