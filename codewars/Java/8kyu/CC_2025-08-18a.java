649e6c6e70baf96df2d5a02f3c81fe2401d54e95
Eva Langerova <langerova_eva@yahoo.com>
2025-08-18 19:44:38 +0200

    add coding challenge

---
+++ codewars/Java/8kyu/CC_2025-08-18a.java
@@ -0,0 +1,10 @@
+/* Returning Strings 
+
+Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
+*/
+
+public class Kata {
+  public static String greet(String name) {
+    return "Hello, " + name + " how are you doing today?";
+  }
+}
