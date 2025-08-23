2a9901fd825b32bb3b48ec15f6afd8ce5e5f212b
Eva Langerova <langerova_eva@yahoo.com>
2025-08-23 20:01:52 +0200

    add coding challenge

---
+++ codewars/Java/8kyu/CC_2025-08-23b.java
@@ -0,0 +1,10 @@
+/* MakeUpperCase 
+
+Write a function which converts the input string to uppercase.
+*/
+
+class Upper {
+  public static String makeUpperCase(String str) {
+    return str.toUpperCase();
+  }
+}
