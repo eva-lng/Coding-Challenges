6f05154b22d1837e5870964ccf7171e67c14ea28
Eva Langerova <langerova_eva@yahoo.com>
2025-08-25 20:23:29 +0200

    add coding challenge

---
+++ codewars/Java/8kyu/CC_2025-08-25a.java
@@ -0,0 +1,24 @@
+/* Beginner Series #2 Clock 
+
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+
+Example:
+h = 0
+m = 1
+s = 1
+
+result = 61000
+
+Input constraints:
+0 <= h <= 23
+0 <= m <= 59
+0 <= s <= 59
+*/
+
+public class Clock {
+  public static int Past(int h, int m, int s) {
+    return (s * 1000) + (m * 60 * 1000) + (h * 3600 * 1000);
+  }
+}
