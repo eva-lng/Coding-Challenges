51715e21f1cca7021c713ddb3854fb953d099e1c
Eva Langerova <langerova_eva@yahoo.com>
2025-09-07 21:31:18 +0200

    add coding challenge

---
+++ codewars/Java/8kyu/CC_2025-09-07.java
@@ -0,0 +1,18 @@
+/* Sum Mixed Array
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+
+import java.util.List;
+
+public class MixedSum {
+	public int sum(List<?> mixed) {	
+		int sum = 0;
+    for (int i = 0; i < mixed.size(); i++) {
+      sum += Integer.parseInt(mixed.get(i) + "");
+    }
+    return sum;
+	}
+}
