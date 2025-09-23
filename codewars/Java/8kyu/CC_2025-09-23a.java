889d548d33a86a1bce6b7f9ab949bf6ff5e2b736
Eva Langerova <langerova_eva@yahoo.com>
2025-09-23 22:28:14 +0200

    add coding challenge

---
+++ codewars/Java/8kyu/CC_2025-09-23a.java
@@ -0,0 +1,13 @@
+/* Grasshopper - Debug sayHello
+
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+Hello, Mr. Spock
+*/
+
+public class GrassHopper {
+  public static String sayHello(String name) {
+    return "Hello, " + name;
+  }
+}
