12d5f3466e790ef6670b3c4a18a43e84772f5a27
Eva Langerova <langerova_eva@yahoo.com>
2025-10-04 22:46:25 +0200

    add coding challenge

---
+++ codewars/Java/8kyu/CC_2025-10-04b.java
@@ -0,0 +1,10 @@
+/* Grasshopper - Terminal game combat function
+
+Create a combat function that takes the player's current health and the amount of damage received, and returns the player's new health. Health can't be less than 0.
+*/
+
+public class Game {
+  public static int combat(int health, int damage) {
+    return health - damage < 0 ? 0 : health - damage;
+  }
+}
