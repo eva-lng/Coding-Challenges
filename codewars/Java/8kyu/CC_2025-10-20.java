304945519b10365bc8cdd7dcc81d8f05ced0a686
Eva Langerova <langerova_eva@yahoo.com>
2025-10-20 17:56:44 +0200

    add coding challenge

---
+++ codewars/Java/8kyu/CC_2025-10-20.java
@@ -0,0 +1,14 @@
+/* Short Long Short
+
+Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+
+For example: (Input1, Input2) --> output
+("1", "22") --> "1221"
+("22", "1") --> "1221"
+*/
+
+public class ShortLongShort {
+    public static String solution(String a, String b) {
+      return a.length() < b.length() ? a + b + a : b + a + b;
+    }
+}
