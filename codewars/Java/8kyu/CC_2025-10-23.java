cc8f868630af08973f85c75cf5b13a88b43fe79b
Eva Langerova <langerova_eva@yahoo.com>
2025-10-23 18:00:05 +0200

    add coding challenge

---
+++ codewars/Java/8kyu/CC_2025-10-23.java
@@ -0,0 +1,15 @@
+/* get character from ASCII Value
+
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+65 --> 'A'
+97 --> 'a'
+48 --> '0
+*/
+
+public class Ascii {
+  public static char getChar(int c) {
+    return (char) c;
+  }
+}
