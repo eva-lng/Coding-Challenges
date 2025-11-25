512dcbc4594448e9e416d9391ce69d6e8b3ea974
Eva Langerova <langerova_eva@yahoo.com>
2025-11-25 13:16:21 +0100

    add coding challenge

---
+++ codewars/Java/7kyu/CC_2025-11-25.java
@@ -0,0 +1,29 @@
+/* Don't give me five!
+
+In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+Examples:
+1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+
+The result may contain fives. ;-)
+The start number will always be smaller than the end number. Both numbers can be also negative!
+*/
+
+import java.util.stream.IntStream;
+
+public class Kata1 {
+  public static int dontGiveMeFive(int start, int end) {
+    return (int) IntStream.rangeClosed(start, end).filter(n -> !String.valueOf(n).contains("5")).count();
+  }
+}
+
+public class Kata2 {
+  public static int dontGiveMeFive(int start, int end) {
+    int count = 0;
+    for (int i = start; i <= end; i++) {
+      if (!String.valueOf(i).contains("5")) count++;
+    }
+    return count;
+  }
+}
