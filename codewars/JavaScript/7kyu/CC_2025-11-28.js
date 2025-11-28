b379a826968e3a384bd95c2a83446c5e6c2d7084
Eva Langerova <langerova_eva@yahoo.com>
2025-11-28 12:52:08 +0100

    add coding challenge

---
+++ codewars/JavaScript/7kyu/CC_2025-11-28.js
@@ -0,0 +1,14 @@
+/* Only one 
+
+Write a function which returns True if ONLY ONE of the boolean flag is True, otherwise return False. If there are no boolean flag, return False
+
+Input	                              Expected Output
+No flags passed in	                False
+True, False, False	                True
+True, False, False, True	          False
+False, False, False, False	        False
+*/
+
+function onlyOne(...args) {
+  return args.filter(Boolean).length === 1;
+}
