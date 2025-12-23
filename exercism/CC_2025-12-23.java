96cf00392dab92810dac93bdb6151c719e039664
Eva Langerova <langerova_eva@yahoo.com>
2025-12-23 18:08:54 +0100

    add coding challenge

---
+++ exercism/CC_2025-12-23.java
@@ -0,0 +1,28 @@
+/* Grains
+
+There once was a wise servant who saved the life of a prince. The king promised to pay whatever the servant could dream up. Knowing that the king loved chess, the servant told the king he would like to have grains of wheat. One grain on the first square of a chessboard, with the number of grains doubling on each successive square.
+
+Instructions:
+Calculate the number of grains of wheat on a chessboard.
+
+A chessboard has 64 squares. Square 1 has one grain, square 2 has two grains, square 3 has four grains, and so on, doubling each time.
+
+Write code that calculates:
+- the number of grains on a given square
+- the total number of grains on the chessboard
+*/
+
+import java.math.BigInteger;
+
+class Grains {
+  BigInteger grainsOnSquare(final int square) {
+    if (square < 1 || square > 64) {
+      throw new IllegalArgumentException("square must be between 1 and 64");
+    }
+    return BigInteger.valueOf(2).pow(square - 1);
+  }
+  
+  BigInteger grainsOnBoard() {
+    return BigInteger.valueOf(2).pow(64).subtract(BigInteger.ONE);
+  }
+}
