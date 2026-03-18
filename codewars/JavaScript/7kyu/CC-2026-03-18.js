ac20e63221b81ca7cd1c282ce6ab4817d6ba4513
Eva Langerova <langerova_eva@yahoo.com>
2026-03-18 18:11:57 +0100

    add coding challenge

---
+++ codewars/JavaScript/7kyu/CC-2026-03-18.js
@@ -0,0 +1,13 @@
+/* Sort with a sorting array
+
+Sort an array according to the indices in another array.
+It is guaranteed that the two arrays have the same size, and that the sorting array has all the required indices.
+
+sort(['x', 'y', 'z'], [1, 2, 0]) => ['z', 'x', 'y']
+    
+sort(['z', 'x', 'y'], [0, 2, 1]) => ['z', 'y', 'x']
+*/
+
+function sort(initialArray, sortingArray) {
+  return initialArray.map((_, i) => initialArray[sortingArray.indexOf(i)]);
+}
