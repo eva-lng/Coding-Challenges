a2fc3dcf059a6fb4dc8f25ea7a3d5e757eceb5a0
Eva Langerova <langerova_eva@yahoo.com>
2026-03-25 20:08:25 +0100

    add coding challenge

---
+++ codewars/JavaScript/6kyu/CC_2026-03-25.js
@@ -0,0 +1,16 @@
+/* Non-even substrings
+
+Given a string of integers, return the number of odd-numbered substrings that can be formed.
+
+For example, in the case of "1341", they are 1, 1, 3, 13, 41, 341, 1341, a total of 7 numbers.
+
+solve("1341") = 7. See test cases for more examples.
+*/
+
+function solve(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] % 2 === 1) count += i + 1;
+  }
+  return count;
+}
