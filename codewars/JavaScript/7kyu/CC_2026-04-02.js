4f3b94d21a871b446e1976d6afeef45931534636
Eva Langerova <langerova_eva@yahoo.com>
2026-04-02 19:55:49 +0200

    add coding challenge

---
+++ codewars/JavaScript/7kyu/CC_2026-04-02.js
@@ -0,0 +1,44 @@
+/* Alphabetical Grid
+
+You need to write a function grid that returns an alphabetical grid of size NxN, where a = 0, b = 1, c = 2....
+
+Examples:
+grid(4)
+a b c d
+b c d e
+c d e f
+d e f g
+
+grid(10)
+a b c d e f g h i j
+b c d e f g h i j k
+c d e f g h i j k l
+d e f g h i j k l m
+e f g h i j k l m n
+f g h i j k l m n o
+g h i j k l m n o p
+h i j k l m n o p q
+i j k l m n o p q r
+j k l m n o p q r s
+
+Notes:
+- After "z" comes "a"
+- If function receive N < 0 should return: null
+*/
+
+function grid(N) {
+  if (N < 0) return null;
+
+  const abc = "abcdefghijklmnopqrstuvwxyz";
+  const res = [];
+
+  for (let i = 0; i < N; i++) {
+    const row = [];
+    for (let j = 0; j < N; j++) {
+      row.push(abc[(i + j) % 26]);
+    }
+    res.push(row.join(" "));
+  }
+
+  return res.join("\n");
+}
