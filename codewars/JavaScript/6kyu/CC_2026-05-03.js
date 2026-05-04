718816a93ae93309d5b82bf4896a821e5d129855
Eva Langerova <langerova_eva@yahoo.com>
2026-05-04 10:30:02 +0200

    add coding challenge

--- codewars/JavaScript/6kyu/CC_2026-05-03.js
+++ codewars/JavaScript/6kyu/CC_2026-05-03.js
@@ -1,6 +1,33 @@
 /* Expressive Objects
 
+Write function evaluate(left, op, right)
+that given two operands (left and right) and an operator (op), will evaluate and return the result.
 
+For example:
+const left = {
+    'multiply': {
+        'add': [1, 2, 3, 4, 5],
+        'subtract': [5, 6, 7, 8, 9],
+        'multiply': [1, 2, 3, 4, 5],
+        'divide': [1, 2, 3, 4, 5]
+    }
+};
+// You will reduce each array based on its key. So you will start of with add(ing) [1,2,3,4,5] => 15
+// then subtract(ing) [5, 6, 7, 8, 9] => -25 etc. Once all arrays have been reduced, you will perform the
+// root key operation on all the reduced arrays. So in this case, you will multiply each reduced array with
+// each other. The order is ALWAYS value of 'add' key first, then 'subtract', then 'multiply' and finally 
+// 'divide'.
+
+const right = {
+    'subtract': {
+        'add': [1, 2, 3, 4, 5],
+        'subtract': [1, 2, 3, 4, 5],
+        'multiply': [1, 2, 3, 4, 5],
+        'divide': [1, 2, 3, 4, 5]
+    }
+};
+
+evaluate(left, 'add', right) === -467.0083333333333
 */
 
 function calcOp(op, arr) {
