35873ec02ed365d7bff757e8cb533510b0526ef6
Eva Langerova <langerova_eva@yahoo.com>
2026-08-04 16:06:54 +0200

    add coding challenge

---
+++ codewars/JavaScript/7kyu/CC_2026-08-04.js
@@ -0,0 +1,24 @@
+/* Halving Sum
+
+Given a positive integer n, calculate the following sum:
+n + n/2 + n/4 + n/8 + ...
+
+All elements of the sum are the results of integer division.
+
+Example:
+25  =>  25 + 12 + 6 + 3 + 1 = 47
+*/
+
+function halvingSum1(n) {
+  if (n === 1) return n;
+  return n + halvingSum1(Math.floor(n / 2));
+}
+
+function halvingSum2(n) {
+  let sum = 0;
+  while (n > 0) {
+    sum += n;
+    n = Math.floor(n / 2);
+  }
+  return sum;
+}
